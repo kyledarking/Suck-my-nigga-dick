dfce4e3086d2b38e4a12a4c016d5b3ffaae0ca2d
kyledarking <167161371+kyledarking@users.noreply.github.com>
2024-04-18 17:11:44 +0800

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -46,7 +46,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "💻 | System prefix: %1\n💬 | Your box chat prefix: %2\n👦 | Created By Mark"
+			myPrefix: "💻 | System prefix: %1\n💬 | Your box chat prefix: %2\n👤 | Created By KYLE BAIT-IT"
 		}
 	},
 
